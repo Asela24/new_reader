dd057455439e082a2a969f6a4c401b1c59956833
Adelina <adeline.selivanovskaia@toptal.com>
2025-01-16 15:22:16 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'test.desu.one', // Замените на ваш IP
-    port: 3000,           // Укажите порт
+    host: "test.desu.one", // Замените на ваш IP
+    port: 3000, // Укажите порт
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        chunkFileNames: "index.js",
+        assetFileNames: ({ names }) => {
+          if (/\.(css)$/.test(names[0] ?? "")) {
+            return "index.css";
+          }
+          return "index[extname]";
+        },
+      },
+    },
+  },
+});
